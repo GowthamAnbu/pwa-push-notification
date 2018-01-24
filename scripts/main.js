44d0462668e6a8e7682ad87323993fdc29b44a4d
Gowtham Anbazhagan <gowthama@tag086.tag.dc>
2018-01-24 13:42:36 +0530

    initial service worker registration

--- scripts/main.js
+++ scripts/main.js
@@ -42,3 +42,21 @@ function urlB64ToUint8Array(base64String) {
   }
   return outputArray;
 }
+
+// STEP:1 initial service worker registration #L47-L62
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
