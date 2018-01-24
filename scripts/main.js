e1b7b790757cfd058fafad52682091597b0e23c7
Gowtham Anbazhagan <gowthama@tag086.tag.dc>
2018-01-24 15:28:06 +0530

    subscribing the user

--- scripts/main.js
+++ scripts/main.js
@@ -32,8 +32,8 @@ let swRegistration = null;
 function urlB64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
+  .replace(/\-/g, '+')
+  .replace(/_/g, '/');
 
   const rawData = window.atob(base64);
   const outputArray = new Uint8Array(rawData.length);
@@ -47,11 +47,9 @@ function urlB64ToUint8Array(base64String) {
 // STEP:1 initial service worker registration
 if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.log('Service Worker and Push is supported');
-
   navigator.serviceWorker.register('sw.js')
   .then(function(swReg) {
     console.log('Service Worker is registered', swReg);
-
     swRegistration = swReg;
     initializeUI();
   })
@@ -65,28 +63,73 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 
 // STEP:2 initializing UI  based on subscription web support; not the user subscription
 function initializeUI() {
+  // STEP:3 click functionality
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      // TODO: Unsubscribe user
+    } else {
+      subscribeUser();
+    }
+  });
   // Set the initial subscription value (ORIGINAL)
   swRegistration.pushManager.getSubscription()
   .then(function(subscription) {
     isSubscribed = !(subscription === null);
-
     if (isSubscribed) {
       console.log('User IS subscribed.');
     } else {
       console.log('User is NOT subscribed.');
     }
-
     updateBtn();
   });
 }
 
 // STEP:2 updating the UI based on the user subscription
 function updateBtn() {
+  // STEP:3 notification denied functionality
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
   if (isSubscribed) {
     pushButton.textContent = 'Disable Push Messaging';
   } else {
     pushButton.textContent = 'Enable Push Messaging';
   }
-
   pushButton.disabled = false;
+}
+
+// STEP:3 Subscribing user
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed.');
+    updateSubscriptionOnServer(subscription);
+    isSubscribed = true;
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+// STEP:3 update subscription on server (the user key is stored into our database) which can then be used for sending the notifications
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails = document.querySelector('.js-subscription-details');
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
 }
